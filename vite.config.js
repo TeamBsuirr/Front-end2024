ba4baaf2a120b4d16bee9efc23412f27312e8921
NikitaNA <vaper20041337@gmail.com>
2024-07-14 08:46:10 +0300

    gh pages 5

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Front-end2024/'  // Здесь указываете ваш путь
 })
