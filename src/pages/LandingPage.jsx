97e0d74f403b07229b2f0048613eaa02130e86b7
NikitaNA <vaper20041337@gmail.com>
2024-10-11 10:53:17 +0300

    add https to vars

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -48,7 +48,7 @@ export default function LandingPage() {
                 <section className="section-background">
                     <div className="section-content">
                         <h1>
-                            {t('main.title.h1')}
+                            httpps{t('main.title.h1')}
                         </h1>
                         <h2> {t('main.title.h2-1')}</h2>
                         <h2> {t('main.title.h2-2')}</h2>
