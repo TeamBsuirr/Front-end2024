18329e0d166ef9497169aa93253d517f9b550cd2
NikitaNA <vaper20041337@gmail.com>
2024-10-13 19:26:14 +0300

    refresh

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,14 @@
-import React, { useEffect, useState } from 'react';
-
-
+import React, { useEffect } from 'react';
 import DefaultLayout from './layouts/DefaultLayout';
-import LandingPage from './pages/LandingPage';
-
-import { createBrowserRouter, Route, RouterProvider, Routes, Navigate, Outlet } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import './utils/globalFunctions';
-
 import { scheduleTokenRefresh } from './utils/tokenService';
 import { HelmetProvider } from 'react-helmet-async';
-
 
 const router = createBrowserRouter([
   { path: '/*', element: <DefaultLayout /> },
 ]);
-
 
 export default function App() {
   useEffect(() => {
