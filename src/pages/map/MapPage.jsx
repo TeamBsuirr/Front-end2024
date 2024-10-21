b6c761fc6891406831b0a56a28079bc2830c38c1
NikitaNA <vaper20041337@gmail.com>
2024-10-21 13:29:36 +0300

    add map 3?

--- src/pages/map/MapPage.jsx
+++ src/pages/map/MapPage.jsx
@@ -7,63 +7,44 @@ import placeService from '../../api/services/placeService';
 import MapUzniki from '../../components/other/MapUzniki';
 import { useTranslation } from 'react-i18next';
 
-
-export default function MapPage({isAdmin=false}) {
+export default function MapPage({isAdmin = false}) {
     const { t } = useTranslation();
     const [arrayOfPlaces, setArrayOfPlaces] = useState(null);
     const [loading, setLoading] = useState(true);
-    const [passedPlace,setPassedPlace]=useState(null);
+    const [passedPlace, setPassedPlace] = useState(null);
 
     useEffect(() => {
         setLoading(true);
 
-
         const queryStringArray = window.location.hash.split('#');
         const idOfPlace = +queryStringArray[queryStringArray.length - 1];
-        
 
         placeService.getAllPlaces()
             .then(data => {
                 setArrayOfPlaces(data);
                 setLoading(false);
-                //console.log(arrayOfPlaces)
-                if(idOfPlace && data){
-                    data.map(obj=>{
-                        if(obj.id === idOfPlace)
-                            setPassedPlace(obj);
-                        
-                    })
-                   
-                }
-                console.log(data)
 
+                if (idOfPlace && data) {
+                    data.forEach(obj => {
+                        if (obj.id === idOfPlace) setPassedPlace(obj);
+                    });
+                }
             })
             .catch(error => {
-                //console.error('Ошибка получения данных концлагерей:', error);
-
-                let errMsg = error.message ? error.message : error;
-
+                let errMsg = error.message || error;
                 notification.error({
                     message: t('errors.front-end.fetch.msg-places'),
                     description: t('errors.front-end.fetch.description') + errMsg
                 });
-
                 setLoading(false);
             });
-
-
-
-    }, []);
-
-
+    }, [t]);
 
     if (loading) {
         return <PageTemplate content={<Spinner size="large" />} />;
     } else if (!arrayOfPlaces) {
         return <NotFound />;
     } else {
-        return (
-            <MapUzniki arrayOfPlaceMarks={arrayOfPlaces} passedPlace={passedPlace} isAdmin={isAdmin} />
-        );
+        return <MapUzniki arrayOfPlaceMarks={arrayOfPlaces} passedPlace={passedPlace} isAdmin={isAdmin} />;
     }
 }
