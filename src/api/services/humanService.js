32011c17e712867be4e8558e6d080e51f8487143
NikitaNA <vaper20041337@gmail.com>
2024-12-01 01:23:42 +0300

    update TO JSON EMPTY

--- src/api/services/humanService.js
+++ src/api/services/humanService.js
@@ -118,26 +118,26 @@ const humanService = {
         if (images.length > 0) {
             images.forEach(id => transformedData.append("images", id));
         } else {
-            transformedData.append("images", []); // Add empty array as a string
+            transformedData.append("images", JSON([])); // Add empty array as a string
         }
 
         if (videos.length > 0) {
             videos.forEach(id => transformedData.append("videos", id));
         } else {
-            transformedData.append("videos", []); // Add empty array as a string
+            transformedData.append("videos", JSON([])); // Add empty array as a string
         }
 
         // Append new images and new videos (files) to transformedData
         if (newImages.length > 0) {
             newImages.forEach(file => transformedData.append("newImages", file));
         } else {
-            transformedData.append("newImages", []); // Add empty array as a string
+            transformedData.append("newImages", JSON([])); // Add empty array as a string
         }
 
         if (newVideos.length > 0) {
             newVideos.forEach(file => transformedData.append("newVideos", file));
         } else {
-            transformedData.append("newVideos", []); // Add empty array as a string
+            transformedData.append("newVideos", JSON([])); // Add empty array as a string
         }
 
 
